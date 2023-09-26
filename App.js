354d5e3d20c6f83cfbdef846b044592e92e60837
Elio Vega <eliosilver@hotmail.com>
2023-09-26 02:18:25 -0500

    cambios project 4

--- App.js
+++ App.js
@@ -1,18 +1,26 @@
 const express = require("express");
 const app = express();
+const port = process.env.PORT || 3001;
+
+require("dotenv").config();
 
 const { validateHTTPMethods } = require("./http-method-middleware");
-
-app.use(express.json());
-app.use(validateHTTPMethods); 
-
 const listViewRouter = require("./list-view-router");
 const listEditRouter = require("./list-edit-router");
+const authRouter = require("./auth-router");
 
+const { authenticateToken } = require("./auth-middleware");
+
+app.use(express.json());
+app.use(validateHTTPMethods);
 app.use("/list-view", listViewRouter);
 app.use("/list-edit", listEditRouter);
+app.use("/login", authRouter);
 
-const port = process.env.PORT || 3001;
+app.get("/ruta-protegida", authenticateToken, (req, res) => {
+  res.json({ message: "Ruta protegida accedida correctamente", user: req.user });
+});
+
 app.listen(port, () => {
   console.log(`Servidor Express en funcionamiento en el puerto ${port}`);
 });
